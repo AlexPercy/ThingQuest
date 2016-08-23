90c9e2b508790baf476b8b3c881097754e918720
Alex Percy <alex_percy@live.co.uk>
2016-08-23 01:02:48 +0100

    changed end turn button graphic added test class

---
+++ Assets/Resources/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	GameController gameController;
+
+	void Start()
+	{
+		gameController = this.gameObject.GetComponent<GameController>();
+	}
+
+	public void Test()
+	{
+		if(gameController.playersTurn)
+		{
+			print("test");
+		}
+	}
+}
